5ec30889979656d6f9c6f9dd7c8c2abe2907eb40
Wade West <wwest81@gmail.com>
2024-10-22 21:53:36 -0500

    Added a function to find Knight moves

--- chess_utils.ts
+++ chess_utils.ts
@@ -1,4 +1,29 @@
 
+export function nextPossibleMoves(tile, rows, cols)
+{
+  let result = new Set();
+  let current = reverseBoardLookup(tile);
+  let row = current[0];
+  let col = current[1];
+  for(let row_offset of [-2, -1, 1, 2])
+  {
+    let next_row = row+row_offset;
+    if(next_row >= 0 && next_row < rows)
+    {
+      for(let col_offset of [-2, -1, 1, 2])
+      {
+        if((row_offset+col_offset)%2==0) continue;
+        let next_col = col+col_offset
+        if(next_col >= 0 && next_col < cols)
+        {
+          result.add(boardLookup(next_row, next_col));
+        }
+      }
+    }
+  }
+  return result;
+}
+
 export function reverseBoardLookup(tile)
 {
   let arr = tile.split('');
