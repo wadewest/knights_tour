cf71f574fde783c878658e2a33e626389facc460
Wade West <wwest81@gmail.com>
2024-10-24 10:09:27 -0500

    Added Queue test for pop

--- tests/queue_test.ts
+++ tests/queue_test.ts
@@ -9,6 +9,13 @@ Deno.test("can push and peek", () => {
   assertEquals(q.peek(), i);
 });
 
+Deno.test("can pop an item", () => {
+  let q = new Queue();
+  let i = "q";
+  q.push(i);
+  assertEquals(q.pop(), i);
+});
+
 Deno.test("can 'get' size", () => {
   let q = new Queue();
   assertEquals(q.size, 0);
